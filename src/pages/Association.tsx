d2ca8e550856cb5f01f1022d0443d33a3a4f7c01
julienjamet <julien.jamet.34@gmail.com>
2023-03-13 17:46:54 +0100

    contact

--- src/pages/Association.tsx
+++ src/pages/Association.tsx
@@ -4,7 +4,7 @@ export const Association = () => {
         <div>
             <div className="presentation">
                 <div className="text">
-                    <h2>L'association</h2>
+                    <h2>L'Association</h2>
 
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla consequat, ex at porttitor convallis, metus ligula mattis odio, at condimentum ipsum dui a ligula. Nam efficitur lorem tincidunt purus ultrices, vel finibus mauris tincidunt. Praesent vitae ligula ut lacus faucibus eleifend. Etiam non dui dolor. Duis id elit posuere odio consectetur placerat. Fusce ultrices massa sit amet tellus molestie, sed blandit ipsum molestie. Aenean nec viverra purus. In hac habitasse platea dictumst. Nunc bibendum posuere vestibulum. Nunc volutpat est est, nec condimentum tortor ornare sollicitudin.
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -2,7 +2,39 @@ export const Contact = () => {
 
     return (
         <div>
-            Contact
+            <div className="contact">
+                <img src="./contact.webp" alt="bar" width="1000px" />
+
+                <div className="text">
+                    <h2>Contact</h2>
+
+                    <div className="main">
+                        <div>
+                            <h3>Adresse</h3>
+                            <p>56 place de l'horloge 30700 Vallabrix</p>
+
+                            <h3>Contact</h3>
+                            <p>abrixbar@gmail.com</p>
+                        </div>
+
+                        <div>
+                            <h3>Horaires d'ouverture</h3>
+
+                            <div className="season">
+                                <p><strong>Horaires d'été (d'avril à octobre)</strong></p>
+
+                                <p>Du mardi au samedi 17h00 - 23h00</p>
+                            </div>
+
+                            <div className="season">
+                                <p><strong>Horaires d'hiver (d'octobre à avril)</strong></p>
+
+                                <p>Mardi et vendredi 17h00 - 19h00</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
